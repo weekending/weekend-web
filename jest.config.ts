058315a82afa7c449c950d06be85232f81952da7
Anna <yorocobe@gmail.com>
2025-02-25 18:37:40 +0900

    chore: vanilla-extract 패키지 추가

--- jest.config.ts
+++ jest.config.ts
@@ -10,6 +10,9 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
+  transform: {
+    "\\.css\\.ts$": "@vanilla-extract/jest-transform",
+  },
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
