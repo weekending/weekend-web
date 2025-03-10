f839f3333f7d01da8176f76bd9b6b52fc5101422
JiSop <jisop.shin@gmail.com>
2025-03-10 18:36:56 +0900

    chore: @vanilla-extract/jest-transform 패키지 추가 및 설정
    vanilla-extract와 jest 테스트 환경 설정

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,9 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
+  transform: {
+    '\\.css\\.ts$': '@vanilla-extract/jest-transform',
+  },
 };
 
 export default createJestConfig(config);
