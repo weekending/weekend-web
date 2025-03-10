631bd02116d1ff60c799a6140256594cb3d50c57
JiSop <jisop.shin@gmail.com>
2025-03-10 16:31:22 +0900

    style: prettier 설정에 맞게 코드 스타일 수정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,16 @@
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
-})
+});
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript", "plugin:prettier/recommended"],
+    extends: [
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:prettier/recommended',
+    ],
   }),
 ];
 
