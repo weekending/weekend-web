b25e322af246b753161636626654f329dafeffd0
JiSop <jisop.shin@gmail.com>
2025-03-28 10:05:21 +0900

    chore: @vanilla-extract 관련 설정 및 스타일 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import '@/src/app/styles/globals.css';
 
 export const metadata: Metadata = {
   title: 'Weekending',
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from 'next';
-import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
-
-const withVanillaExtract = createVanillaExtractPlugin();
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withVanillaExtract(nextConfig);
+export default nextConfig;
